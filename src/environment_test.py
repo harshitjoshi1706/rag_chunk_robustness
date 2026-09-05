379256ac737acf6102f267c1f75f40da06ad9bda
Harshit Joshi <harshitjoshi1706@gmail.com>
2026-09-05 19:46:14 +0530

    Add environment sanity test

---
+++ src/environment_test.py
@@ -0,0 +1,8 @@
+import sys
+import numpy as np
+import pandas as pd
+
+print("Python:", sys.version.split()[0])
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Environment test passed.")
